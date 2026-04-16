5400c2d4243d529e2b303d12ba54c09723c31748
Seerat Kaur <seeratk0189@gmail.com>
2026-04-16 18:32:47 +0530

    Fixed CORS

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5005;
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.use('/api/products', productRoutes);
